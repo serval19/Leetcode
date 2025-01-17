113cb2f3f28ea21e979bb9204db557991465dce3
Diwa Tomy <diwatomy04@gmail.com>
2025-01-18 01:50:12 +0530

    new

--- closestnumbertozero.py
+++ closestnumbertozero.py
@@ -1,7 +1,10 @@
 class Solution:
     def findClosestNumber(self, nums: List[int]) -> int:
-        x=abs(nums[0])
+        x=nums[0]
         for i in nums:
-            if abs(i)<x:
-                x=abs(i)
+            if abs(i)<abs(x):
+                x=i
+        if x<0 and abs(x) in nums:
+            return abs(x)
         return x
+            
