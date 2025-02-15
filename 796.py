46084984e5b47f838ad9b9e399b8b632cda23443
Diwa Tomy <144988434+serval19@users.noreply.github.com>
2025-02-16 00:27:40 +0530

    Create 796.py

---
+++ 796.py
@@ -0,0 +1,10 @@
+# Rotate String
+class Solution:
+    def rotateString(self, s: str, goal: str) -> bool:
+        for i in range(len(s)):
+            s=s[1:]+s[0]
+            if s==goal:
+                return True
+        else:
+            return False
+        
